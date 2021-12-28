3f9df4cc673c324f188674eafa0fdd382ae2b960
Your Name <email@example.com>
2021-12-27 19:56:53 -0800

    stuff

--- docs/js/egg.js
+++ docs/js/egg.js
@@ -9,6 +9,8 @@ const egg = `
         background: #f5f5f5;
         border: 2px solid black;
         border-radius: 5px;
+        -webkit-box-shadow: -1px 2px 11px -4px #000000; 
+        box-shadow: -1px 2px 11px -4px #000000;
     }
 
     .recipe h1 {
@@ -24,6 +26,8 @@ const egg = `
         background: white;
         text-align: center;
         font-size: 0.9em;
+        margin-top: 16px;
+        margin-bottom: 11px;
     }
 
     .recipe table td {
@@ -74,7 +78,7 @@ const egg = `
 </ul>
 <h2>Instructions</h2>
 <ul class="nums">
-    <li>(optional) Prep the spinch by chopping it into small shards</li>
+    <li>(optional) Prep the spinach by chopping it into small shards</li>
     <li>Crack the eggs into the bowl, preferably with only one hand</li>
     <li>Add a healthy amount of salt and pepper to the bowl</li>
     <li>Vigorously whisk the eggs with the fork in a spinning motion with the axis of rotation about the length of the fork</li>
@@ -84,7 +88,7 @@ const egg = `
     <li>Stir!</li>
     <li>Stir!</li>
     <li>Stir!</li>
-    <li>Don't stop stirring the eggs the entire time! Don't just stir either! Scrape eggs off the bottom of the pan before they have a chance to get stuck. No one piece of egg should stay in the same place for more than a second.</li>
+    <li>Don't stop stirring the eggs the entire time! Don't just stir either! Scrape eggs off the bottom of the pan before they have a chance to get stuck. No one piece of egg should stay in the same place for more than a second</li>
     <li>(optional) Once the eggs start to form up a bit add the spinach</li>
     <li>The eggs will retain a good amount of heat and continue cooking even after you take them out of the pan, so take them out when they look about 80% done (firm but watery)</li>
     <li>(optional) Top with cheese</li>
